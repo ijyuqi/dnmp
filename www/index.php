097c422efdd700e478b1017527a1adbf8042323e
ijyuqi <904932898@qq.com>
2019-06-30 12:00:29 +0800

    添加测试文件

---
+++ www/index.php
@@ -0,0 +1,10 @@
+<?php
+echo '<p>php环境搭建成功</p>';
+try {
+    new PDO('mysql:host=mydb;dbname=mysql', 'root', '123456');//host地址直接使用link指定的标签mydb
+    echo '<p>mysql连接成功</p>';
+} catch (Exception $e) {
+    echo '<p>mysql连接失败 : '.$e->getMessage().'</p>';
+} finally {
+
+}
