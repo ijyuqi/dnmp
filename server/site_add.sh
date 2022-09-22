e99cf18d7ca40c05f25be9c5c731f056059a3660
ijyuqi <904932898@qq.com>
2022-09-22 15:06:12 +0800

    fix:添加站点脚本更新

--- server/site_add.sh
+++ server/site_add.sh
@@ -4,36 +4,42 @@ echo "添加站点"
 
 echo -n "输入站点域名:(localhost):"
 read DOMAIN_NAME
-if [ ! $DOMAIN_NAME ]; then
-	$DOMAIN_NAME = 'localhost'
+if [[ -z $DOMAIN_NAME ]]; then
+	DOMAIN_NAME='localhost'
 fi
 
-echo -n "输入站点文件名:"
+echo -n "输入站点文件名:(localhost):"
 read SITE_FILENAME
-if [ ! -d $SITE_FILENAME ]; then
+if [[ -z $SITE_FILENAME ]]; then
+	SITE_FILENAME='localhost'
+fi
+
+# 创建文件夹
+if [[ ! -d $SITE_FILENAME ]]; then
 	mkdir -p ./../www/$SITE_FILENAME
-	echo " the site filename which you want create"
-	exit
+	echo "创建文件夹${SITE_FILENAME}"
 fi
 
 echo -n "输入端口号:(默认80):"
 read PORT
-if [ ! $PORT ]; then
-	$PORT = '80'
+if [[ -z $PORT ]]; then
+	PORT=80
 fi
 
 cd ./nginx/
-sed "s/@DOMAIN_NAME/${DOMAIN_NAME}/g" localhost.templet.conf | sed "s/@SITE_FILENAME/${SITE_FILENAME}/g" | sed "s/@PORT/${PORT}/g" > ./conf.d/${SITE_FILENAME}.conf
 
-echo "Create a domain name: $DOMAIN_NAME"
-echo "Create site folder: $SITE_FILENAME"
-echo "Site port number: $PORT"
+sed "s/@DOMAIN_NAME/${DOMAIN_NAME}/g" localhost.templet.conf | sed "s/@SITE_FILENAME/${SITE_FILENAME}/g" | sed "s/@PORT/${PORT}/g" > ./conf.d/${SITE_FILENAME}_${PORT}.conf
+
+echo "站点域名: $DOMAIN_NAME"
+echo "站点文件夹: $SITE_FILENAME"
+echo "站点端口号: $PORT"
+
 echo "配置全局配置文件.env"
-sed -i "/NGINX_HTTP_HOST_PORT/a\NGINX_HTTP_HOST_PORT_$PORT = $PORT" .env
+echo "NGINX_HTTP_HOST_PORT_${PORT}=${PORT}"
 
 
+#sed -i "/NGINX_VERSION=latest/a\NGINX_HTTP_HOST_PORT_${PORT}=${PORT}" .env
+sed -i '/NGINX_VERSION=latest/a\NGINX_HTTP_HOST_PORT_12=12' .env
 
 echo "Create a successful!"
 
-#docker-compose stop
-#docker-compose up -d
